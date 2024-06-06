34bccf79a33df6de07ae441b053e83a49d2710d8
TingjiaInFuture <109398380+TingjiaInFuture@users.noreply.github.com>
2024-06-06 21:25:22 +0800

    bugfix: invaild q0,F

--- nd.py
+++ nd.py
@@ -1,5 +1,5 @@
-def nfa_to_dfa(nfa):
-    initial_state = set([0])
+def nfa_to_dfa(nfa, initial_state):
+    initial_state = set([initial_state])
     dfa = {str(initial_state): {}}
     unmarked = [initial_state]
 
@@ -20,7 +20,7 @@ def nfa_to_dfa(nfa):
     return dfa
 
 def tuple_to_dict(nfa_tuple):
-    Q, Σ, δ, q0, F = nfa_tuple
+    Q, Σ, δ = nfa_tuple
     nfa_dict = {}
     for state in Q:
         nfa_dict[state] = {}
@@ -36,9 +36,7 @@ def dict_to_tuple(dfa_dict):
     for state, transitions in dfa_dict.items():
         for symbol, next_state in transitions.items():
             δ[frozenset(eval(state)), symbol] = frozenset(eval(next_state))  # 将集合转换为frozenset
-    q0 = next(iter(Q))
-    F = {state for state in Q if any(next_state in Q for next_state in dfa_dict[str(state)].values())}
-    return Q, Σ, δ, q0, F
+    return Q, Σ, δ
 
 def main():
     # 从命令行接收五元组
@@ -49,22 +47,34 @@ def main():
     F = set(eval(input("请输入接受状态集F(例如:{2}):")))
 
     # 将五元组转换为字典形式的NFA
-    nfa = tuple_to_dict((Q, Σ, δ, q0, F))
+    nfa = tuple_to_dict((Q, Σ, δ))
     print("NFA:")
     print(nfa)
 
 
     # 将NFA转换为DFA
-    dfa = nfa_to_dfa(nfa)
+    dfa = nfa_to_dfa(nfa, q0)
     print("DFA:")
     print(dfa)
 
+    # DFA 的终止状态集合初始化为空
+    dF = set()
+
+    # 遍历 DFA 的每个状态
+    for state in dfa:
+        # 将状态从字符串转换回集合
+        state_set = eval(state)
+        # 如果该状态集合与 NFA 的终止状态集合有交集
+        if state_set & F:
+            # 那么这个状态就是 DFA 的一个终止状态
+            dF.add(frozenset(state_set))
 
 
     # 将字典形式的DFA转换回五元组形式
     dfa_tuple = dict_to_tuple(dfa)
     print("DFA(五元组形式）:")
-    print(dfa_tuple)
+    print((dfa_tuple[0], dfa_tuple[1], dfa_tuple[2], {q0}, dF))
+
 
 if __name__ == "__main__":
     main()
